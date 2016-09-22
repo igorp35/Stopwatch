6360e22e6861366944b9641c4d43acded42d6987
IgorP <igorp35@gmail.com>
2016-09-22 08:50:57 +0400

    StopWatch-0.1

--- app/src/main/java/com/example/igor_p/stopwatch/StopWatchActivity.java
+++ app/src/main/java/com/example/igor_p/stopwatch/StopWatchActivity.java
@@ -1,13 +1,69 @@
 package com.example.igor_p.stopwatch;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.TextView;
 
-public class StopWatchActivity extends AppCompatActivity {
+public class StopWatchActivity extends Activity {
+
+    private boolean running;
+    private int seconds;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop_watch);
+        runTime();
+    }
+
+
+
+    public void onClickStart(View v){
+
+        running = true;
+
+    }
+
+    public void onClickStop(View v){
+
+        running = false;
+
+    }
+
+    public void onClicReset(View v){
+
+        running = false;
+        seconds = 0;
+
+    }
+
+    private void runTime(){
+
+        final TextView textTime = (TextView)findViewById(R.id.time_view);
+
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+
+                int hours = seconds / 3600;
+                int minutes = (seconds %3600) / 60;
+                int secs = seconds %60;
+
+                String time = String.format("%d:%02d:%02d",hours,minutes,secs);
+                textTime.setText(time);
+
+                if(running){
+
+                    seconds++;
+                }
+
+                handler.postDelayed(this, 1000);
+            }
+
+        });
+
     }
 }
