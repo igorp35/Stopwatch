f9caf605ba2d91a494a9e49f907708262dab3e0c
IgorP <igorp35@gmail.com>
2016-09-26 08:58:01 +0400

    StopWatch-0.2

--- app/src/main/java/com/example/igor_p/stopwatch/StopWatchActivity.java
+++ app/src/main/java/com/example/igor_p/stopwatch/StopWatchActivity.java
@@ -10,60 +10,55 @@ public class StopWatchActivity extends Activity {
 
     private boolean running;
     private int seconds;
-
+//================================================================================================//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop_watch);
+        if(savedInstanceState != null){
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+        }
         runTime();
     }
-
-
+//================================================================================================//
 
     public void onClickStart(View v){
-
         running = true;
-
     }
 
     public void onClickStop(View v){
-
         running = false;
-
     }
 
     public void onClicReset(View v){
-
         running = false;
         seconds = 0;
-
     }
-
+//================================================================================================//
     private void runTime(){
-
         final TextView textTime = (TextView)findViewById(R.id.time_view);
-
         final Handler handler = new Handler();
         handler.post(new Runnable() {
             @Override
             public void run() {
-
                 int hours = seconds / 3600;
                 int minutes = (seconds %3600) / 60;
                 int secs = seconds %60;
-
                 String time = String.format("%d:%02d:%02d",hours,minutes,secs);
                 textTime.setText(time);
-
                 if(running){
-
                     seconds++;
                 }
-
                 handler.postDelayed(this, 1000);
             }
 
         });
-
     }
+//================================================================================================//
+  public void onSaveInstanceState(Bundle savedInstanceState){
+      savedInstanceState.putInt("seconds", seconds);
+      savedInstanceState.putBoolean("running", running);
+  }
+//================================================================================================//
 }
